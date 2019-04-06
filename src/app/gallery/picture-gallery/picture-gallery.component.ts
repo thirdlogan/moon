ea7ab405539172cc7fd1ffffe58c56bb2b9f25a3
sidspencer <sidspencer@gmail.com>
2019-04-06 17:46:50 -0500

    Simple UI layout.

--- src/app/gallery/picture-gallery/picture-gallery.component.ts
+++ src/app/gallery/picture-gallery/picture-gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaFile } from 'src/app/core/classes/media-file';
+import { GalleryService } from 'src/app/core/services/gallery.service';
 
 @Component({
   selector: 'app-picture-gallery',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picture-gallery.component.css']
 })
 export class PictureGalleryComponent implements OnInit {
+  public pics: MediaFile[] = <MediaFile[]>[];
 
-  constructor() { }
+  constructor(private galleryService: GalleryService) { }
 
   ngOnInit() {
+    this.galleryService.fetchPics({}).subscribe((mediaFiles: MediaFile[]) => {
+      this.pics = mediaFiles;
+    }, (err) => {
+      console.log(err);
+    })
   }
 
 }
